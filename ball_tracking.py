857a02e07369878d3d20f5a1e02987d8cd334853
csites <chuck.sites@altour.com>
2023-07-05 14:35:43 -0400

    Move the rolling fiction estimate to after the fisheye and perspective corrections

--- ball_tracking.py
+++ ball_tracking.py
@@ -1071,29 +1071,6 @@ while True:
                                         print("Ball Left. Position: "+str(center))
                                         left = True
                                         endPos = center
-# FRICTION_ESTIMATE        
-                                        if ( x > coord[3][0]+25): #Make sure we are past the start line and far enough that ball is rolling (not skipping or sliping)
-                                          if (V_started == 0):
-                                            T_started = tim2  # First pass
-                                            V_started = x
-                                            print("Friction 0: V_started = "+str(V_started)+" T_started = "+str(T_started))
-                                            D_initial = 0
-                                            D_final = 0
-                                          elif ( x > V_started + 100 ):
-                                            if (D_initial == 0):  # initial sample
-                                              T_initial = tim2
-                                              D_initial = x
-                                              V_initial = ((x - V_started) / (pixelmmratio * 1000)) / (tim2 - T_started) # meters/sec 
-                                              print("Friction 1: V_initial = "+str(V_initial)+" T_Initial = "+str(T_initial))  
-                                            elif ((x > D_initial + 100) and D_final == 0):  # 100 pixels more than the initial x.
-                                              T_final = tim2
-                                              D_final = x
-                                              print("D_final = x:"+str(x)+" - D:"+str(D_initial)+" / Pix:"+str(pixelmmratio)+"* 1000 / ("+str(tim2 - T_initial)+") ")   
-                                              V_final = ((x - D_initial) / (pixelmmratio * 1000)) / (tim2 - T_initial) #  We should have everything for stimp
-                                              print("Friction 2: V_final = "+str(V_final)+" T_final = "+str(T_final)+" pixelmmratio = "+str(pixelmmratio)) 
-                                              U_friction = compute_rolling_friction (V_initial, V_final, (x - D_initial) / (pixelmmratio * 1000))
-                                              print("Coefficient_of_rolling_friction",U_friction)
-# END FRICTION_ESTIMATE                                             
                                                                                 
 # FISHEYE                                
                                         # CBS: This is where we do fisheye correction on the two corredinates (startPos, and endPos).
@@ -1130,6 +1107,30 @@ while True:
                                             startPos = s
                                             endPos = e   
 # END PERSPECTIVE CORRECTION
+# FRICTION_ESTIMATE        
+                                        if ( endPos[0] > coord[3][0]+25): #Make sure we are past the start line and far enough that ball is rolling (not skipping or sliping)
+                                          if (V_started == 0):
+                                            T_started = tim2  # First pass
+                                            V_started = endPos[0] # First corrected x position.
+                                            print("Friction 0: V_started = "+str(V_started)+" T_started = "+str(T_started))
+                                            D_initial = 0
+                                            D_final = 0
+                                          elif ( endPos[0] > V_started + 100 ):
+                                            if (D_initial == 0):  # initial sample
+                                              T_initial = tim2
+                                              D_initial = endPos[0]
+                                              V_initial = ((D_initial - V_started) / (pixelmmratio * 1000)) / (tim2 - T_started) # meters/sec 
+                                              print("Friction 1: V_initial = "+str(V_initial)+" T_Initial = "+str(T_initial))  
+                                            elif (( endPos[0] > D_initial + 100) and D_final == 0):  # 100 pixels more than the initial x.
+                                              T_final = tim2
+                                              D_final = endPos[0]
+                                              print("D_final = x:"+str(x)+" - D:"+str(D_initial)+" / Pix:"+str(pixelmmratio)+"* 1000 / ("+str(tim2 - T_initial)+") ")   
+                                              V_final = ((D_final - D_initial) / (pixelmmratio * 1000)) / (tim2 - T_initial) #  We should have everything for stimp
+                                              print("Friction 2: V_final = "+str(V_final)+" T_final = "+str(T_final)+" pixelmmratio = "+str(pixelmmratio)) 
+                                              U_friction = compute_rolling_friction (V_initial, V_final, (D_final - D_initial) / (pixelmmratio * 1000))
+                                              print("Coefficient_of_rolling_friction",U_friction)
+# END FRICTION_ESTIMATE                                             
+
                                         a = endPos[0] - startPos[0]
                                         b = endPos[1] - startPos[1]
                                         distanceTraveled = math.sqrt( a*a + b*b )
